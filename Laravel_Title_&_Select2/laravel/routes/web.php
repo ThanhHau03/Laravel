aa5fdb43678a08b059a8ee6dab40ba6d12e7c49f
Thanh Hậu <129538148+ThanhHau03@users.noreply.github.com>
2024-03-13 23:55:26 +0700

    Update project Laravel Select2
    title & ajax select2

---
+++ Laravel_Title_&_Select2/laravel/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\CourseController;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('courses', [CourseController::class, 'index']);
+//Route::get('courses/create', [CourseController::class, 'create'])->name('course.create');
+//Route::post('courses/create', [CourseController::class, 'store'])->name('course.store');
+
+//Route::group(['prefix' => 'courses', 'as' => 'course.'], function() {
+//    Route::get('/', [CourseController::class, 'index'])->name('index');
+//    Route::get('/create', [CourseController::class, 'create'])->name('create');
+//    Route::post('/create', [CourseController::class, 'store'])->name('store');
+//    Route::delete('/destroy/{course}', [CourseController::class, 'destroy'])->name('destroy');
+//    Route::get('/edit/{course}', [CourseController::class, 'edit'])->name('edit');
+//    Route::put('/edit/{course}', [CourseController::class, 'update'])->name('update');
+//});
+
+Route::resource('courses', CourseController::class)->except([
+    'show',
+]);
+
+// viết sau sẽ ghi đè lên thằng viết trước
+Route::get('courses/api', [CourseController::class, 'api'])->name('courses.api');
+Route::get('courses/api/name', [CourseController::class, 'apiName'])->name('courses.api.name');
+
+Route::get('test', function(){
+    return view('layout.master');
+});
